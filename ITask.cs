3ec2b4b5a1c439332883a87699c4fb9e66cc18f8
Mansura Khajra <S00265100@atu.ie>
2026-03-16 10:52:57 +0000

    edited TaskItem class aded refference

--- ITask.cs
+++ ITask.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
 
 namespace Project
 {
@@ -11,7 +8,7 @@ namespace Project
         string Title { get; set; }
         DateTime DueDate { get; set; }
         bool IsCompleted { get; set; }
+
         bool IsOverdue();
-        //this th jj
     }
 }
--- TaskItem.cs
+++ TaskItem.cs
@@ -1,14 +1,28 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Project
 {
-    internal class TaskItem
+    public class TaskItem : ITask
     {
+        [Key]
+        public int Id { get; set; }
+
         public string Title { get; set; }
+
         public string Description { get; set; }
+
         public string Priority { get; set; }
+
         public string Category { get; set; }
+
         public DateTime DueDate { get; set; }
+
         public bool IsCompleted { get; set; }
+
+        public bool IsOverdue()
+        {
+            return DateTime.Now > DueDate && !IsCompleted;
+        }
     }
 }
