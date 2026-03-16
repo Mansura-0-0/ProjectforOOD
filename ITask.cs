37e8fc163484afb978a15f0bc65e6b5c274094b2
Mansura Khajra <S00265100@atu.ie>
2026-03-16 10:23:42 +0000

    aded a new class ITask

---
+++ ITask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    public interface ITask
+    {
+        string Title { get; set; }
+        DateTime DueDate { get; set; }
+        bool IsCompleted { get; set; }
+        bool IsOverdue();
+        //this th jj
+    }
+}
